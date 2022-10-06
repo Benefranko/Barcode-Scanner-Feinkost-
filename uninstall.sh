926d9ad454145be13d8e57e55d1128bea20a9aa4
M4RKUS28 <git+markus@obermui.de>
2022-10-06 20:02:33 +0200

    v3.5.0 ka

--- uninstall.sh
+++ uninstall.sh
@@ -70,12 +70,28 @@ fi
 
 echo "Deaktiviere Feature Herunterfahren und Neustarten über Webserver..."
 
-if sudo sed -i.bak "/${SUDO_USER} ALL=(ALL) NOPASSWD: /sbin/reboot, /sbin/shutdown/d" "/etc/sudoers"; then
-  echo -e "    -> OK\n"
-else
-  echo "    -> FAILED -> restore..."
+if text=$(sudo grep -v "${SUDO_USER}\ ALL=(ALL)\ NOPASSWD:\ /sbin/reboot,\ /sbin/shutdown" "/etc/sudoers"); then
+  print "$text"
+  sleep 10
+  [ -e "/etc/sudoers.bak" ] && sudo rm "/etc/sudoers.bak" && echo "Lösche altes Backup"
+  if mv "/etc/sudoers" "/etc/sudoers.bak"; then
+      if echo "$text" | sudo tee "/etc/sudoers"; then
+        echo -e "    -> OK\n"
+      else
+            echo "    -> FAILED to write new file -> restore..."
+            [ -e "/etc/sudoers" ] && sudo rm "/etc/sudoers"
+            mv "/etc/sudoers.bak" "/etc/sudoers"
+      fi
+  else
+    echo "    -> FAILED to create backup -> restore..."
+    [ -e "/etc/sudoers" ] && sudo rm "/etc/sudoers"
+    mv "/etc/sudoers.bak" "/etc/sudoers"
+  fi
 
+else
+  echo "    -> FAILED grep failed skipp"
 fi
+grep -v "^${SUDO_USER} ALL=(ALL) NOPASSWD: /sbin/reboot, /sbin/shutdown$" "/etc/sudoers" > filename2; mv filename2 filename
 
 echo -e "\nFINISHED"
 
